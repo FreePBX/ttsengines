e0fbd635768bc97f8df6839119b0a25355a19cc2
Andrew Nagy <andrew.nagy@the159.com>
2019-02-21 23:43:05 -0500

    [Module Tag script: ttsengines 15.0.4.2] fixing legacy restore

--- Restore.php
+++ Restore.php
@@ -2,26 +2,14 @@
 namespace FreePBX\modules\Ttsengines;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-  public function runRestore($jobid){
-      $configs = $this->getConfigs();
-      foreach ($configs as $engine) {
-        $this->FreePBX->Ttsengines->addById($engine['id'],$engine['name'],$engine['path']);
-      }
-  }
+	public function runRestore($jobid){
+			$configs = $this->getConfigs();
+			foreach ($configs as $engine) {
+				$this->FreePBX->Ttsengines->addById($engine['id'],$engine['name'],$engine['path']);
+			}
+	}
 
-  public function processLegacy($pdo, $data, $tables, $unknownTables, $tmpfiledir){
-    $tables = array_flip($tables+$unknownTables);
-    if(!isset($tables['ttsengines'])){
-      return $this;
-    }
-    $bmo = $this->FreePBX->Ttsengines;
-    $bmo->setDatabase($pdo);
-    $configs = $bmo->listAll();
-    $bmo->resetDatabase();
-    foreach ($configs as $engine) {
-      $bmo->add($engine['name'], $engine['path']);
-    }
-    return $this;
-  }
-
+	public function processLegacy($pdo, $data, $tables, $unknownTables){
+		$this->restoreLegacyDatabase($pdo);
+	}
 }
--- Ttsengines.class.php
+++ Ttsengines.class.php
@@ -6,15 +6,15 @@ use PDO;
 
 class Ttsengines extends FreePBX_Helpers implements BMO {
 
-    public function install() {
-        $entry = $this->Database->query("SELECT name FROM ttsengines where name = 'flite'");
-        if(!$entry){
-            $this->Database->query("INSERT INTO ttsengines (name, path) values('flite', '/usr/bin/flite')");
-        }
-    }
+	public function install() {
+		$entry = $this->Database->query("SELECT name FROM ttsengines where name = 'flite'");
+		if(!$entry){
+			$this->Database->query("INSERT INTO ttsengines (name, path) values('flite', '/usr/bin/flite')");
+		}
+	}
 
 	public function uninstall() {}
-    public function doConfigPageInit($page) {}
+	public function doConfigPageInit($page) {}
 
 	public function getActionBar($request) {
 		$buttons = array();
@@ -43,87 +43,87 @@ class Ttsengines extends FreePBX_Helpers implements BMO {
 				if (empty($request['edit'])) {
 					unset($buttons['delete']);
 				}
-            break;
-            default:
-                $buttons = [];
-            break;
+			break;
+			default:
+				$buttons = [];
+			break;
 		}
 		return $buttons;
 	}
   public function getEngine($id){
-    $sql = 'SELECT * FROM ttsengines WHERE id = :id';
-    $stmt = $this->FreePBX->Database->prepare($sql);
-    $stmt->execute(array(':id' => $id));
-    $ret = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    return $ret[0];
+	$sql = 'SELECT * FROM ttsengines WHERE id = :id';
+	$stmt = $this->FreePBX->Database->prepare($sql);
+	$stmt->execute(array(':id' => $id));
+	$ret = $stmt->fetchAll(PDO::FETCH_ASSOC);
+	return $ret[0];
   }
 	public function ajaxRequest($command, &$setting) {
-        if($command === 'getJSON'){
-            return true;
-        }
-        return false;
-    }
+		if($command === 'getJSON'){
+			return true;
+		}
+		return false;
+	}
 
 	public function ajaxHandler(){
-        if($_REQUEST['command'] === 'getJSON' && $_REQUEST['jdata'] === 'grid') {
-            return $this->listAll();
-        }
-        return false;
-    }
+		if($_REQUEST['command'] === 'getJSON' && $_REQUEST['jdata'] === 'grid') {
+			return $this->listAll();
+		}
+		return false;
+	}
 
 	public function listAll(){
 		$sql = "SELECT * FROM ttsengines";
 		$stmt = $this->FreePBX->Database->prepare($sql);
 		$stmt->execute();
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
+	}
 
-    public function add($name, $path){
-        $sql = "INSERT INTO ttsengines (name, path) values(:name, :path)";
-        $stmt = $this->FreePBX->Database->prepare($sql);
-        $stmt->execute([
-            ':name' => $name,
-            ':path' => $path,
-        ]);
-        return $this;
-    }
+	public function add($name, $path){
+		$sql = "INSERT INTO ttsengines (name, path) values(:name, :path)";
+		$stmt = $this->FreePBX->Database->prepare($sql);
+		$stmt->execute([
+			':name' => $name,
+			':path' => $path,
+		]);
+		return $this;
+	}
 
-    public function addById($id, $name, $path){
-        $sql = "INSERT INTO ttsengines (id, name, path) values(:id, :name, :path)";
-        $stmt = $this->FreePBX->Database->prepare($sql);
-        $stmt->execute([
-            ':id' => $id,
-            ':name' => $name,
-            ':path' => $path,
-        ]);
-        return $this;
-    }
+	public function addById($id, $name, $path){
+		$sql = "INSERT INTO ttsengines (id, name, path) values(:id, :name, :path)";
+		$stmt = $this->FreePBX->Database->prepare($sql);
+		$stmt->execute([
+			':id' => $id,
+			':name' => $name,
+			':path' => $path,
+		]);
+		return $this;
+	}
 
-    public function delete($id){
-        $sql = "DELETE FROM ttsengines WHERE id = :id";
-        $stmt = $this->FreePBX->Database->prepare($sql);
-        $stmt->execute([
-            ':id' => $id,
-        ]);
-        return $this;
-    }
+	public function delete($id){
+		$sql = "DELETE FROM ttsengines WHERE id = :id";
+		$stmt = $this->FreePBX->Database->prepare($sql);
+		$stmt->execute([
+			':id' => $id,
+		]);
+		return $this;
+	}
 
-    public function update($id, $name, $path){
-        $sql = "UPDATE ttsengines SET name = :name, path = :path WHERE id = :id";
-        $stmt = $this->FreePBX->Database->prepare($sql);
-        $stmt->execute([
-            ':id' => $id,
-            ':name' => $name,
-            ':path' => $path,
-        ]);
-        return $this;
-    }
+	public function update($id, $name, $path){
+		$sql = "UPDATE ttsengines SET name = :name, path = :path WHERE id = :id";
+		$stmt = $this->FreePBX->Database->prepare($sql);
+		$stmt->execute([
+			':id' => $id,
+			':name' => $name,
+			':path' => $path,
+		]);
+		return $this;
+	}
 
 	public function getRightNav($request) {
 		if(isset($request['view'])){
 			return load_view(__DIR__.'/views/rnav.php');
 		}
-    }
+	}
 
 	public function setDatabase($pdo){
 		$this->Database = $pdo;
